fba79c00a016f6bb3cd51730364e8e5d4f06ec14
Billy Jefferson <biljef232@gmail.com>
2020-12-11 09:44:18 +0700

    PraktikumWeek13_BillyJefferson

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.btnProses = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(13, 19);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(92, 17);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Input Nama : ";
+            // 
+            // textInput
+            // 
+            this.textInput.Location = new System.Drawing.Point(12, 61);
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(248, 22);
+            this.textInput.TabIndex = 1;
+            // 
+            // btnProses
+            // 
+            this.btnProses.Location = new System.Drawing.Point(16, 108);
+            this.btnProses.Name = "btnProses";
+            this.btnProses.Size = new System.Drawing.Size(211, 81);
+            this.btnProses.TabIndex = 2;
+            this.btnProses.Text = "Proses";
+            this.btnProses.UseVisualStyleBackColor = true;
+            this.btnProses.Click += new System.EventHandler(this.btnProses_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(13, 271);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(63, 17);
+            this.lblAnswer.TabIndex = 3;
+            this.lblAnswer.Text = "[EMPTY]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnProses);
+            this.Controls.Add(this.textInput);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form Pengaturan Warna";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.Button btnProses;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProses_Click(object sender, EventArgs e)
+        {
+            if (lblAnswer.Text == "[EMPTY]")
+            {
+                lblAnswer.Text = textInput.Text;
+            }
+            if (lblAnswer.Text != "[EMPTY]" && textInput.Text == "DELETE")
+            {
+                lblAnswer.Text = "[EMPTY]";
+            }
+            if (lblAnswer.Text != "[EMPTY]" && textInput.Text == "HIDE")
+            {
+                lblAnswer.Hide();
+            }
+            if (lblAnswer.Text != "[EMPTY]" && textInput.Text == "SHOWN")
+            {
+                lblAnswer.Visible = true;
+            }
+            if (lblAnswer.Text != "[EMPTY]" && textInput.Text == "RED")
+            {
+                lblAnswer.ForeColor = Color.Red;
+            }
+            if (lblAnswer.Text != "[EMPTY]" && textInput.Text == "GREEN")
+            {
+                lblAnswer.ForeColor = Color.Green;
+            }
+            if (lblAnswer.Text != "[EMPTY]" && textInput.Text == "BLUE")
+            {
+                lblAnswer.ForeColor = Color.Blue;
+            }
+        }
+    }
+}
+        
+    
